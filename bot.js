b4e07cb6bf720084ddb14944f4428964a5ee27e2
abdo1st <42328037+abdo1st@users.noreply.github.com>
2018-10-07 03:52:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,48 +12,6 @@ const prefix = "."
 
 
 
-var dat = JSON.parse(fs.readFileSync('./invite.json', 'utf8'));
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("490882512290316288")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'الترحيب');
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("490882512290316288")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
- channel.send(`تم دعوتك من قبل ${Invite.inviter}`)            
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
-});
-
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
