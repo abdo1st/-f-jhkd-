f34199089e007b18134e70bbb6c8ea6254678a42
abdo1st <42328037+abdo1st@users.noreply.github.com>
2018-10-07 04:02:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "."
 const dateformat = require('dateformat');
 const canvas = require('canvas');
 const jimp = require('jimp');
